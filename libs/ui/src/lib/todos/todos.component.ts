6b8961f8fec02f9568e5c1790406ce7d5047b09f
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-08-16 12:20:40 +0200

    basic html/css imported from original app: file to extend

---
+++ libs/ui/src/lib/todos/todos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'front-nx-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
